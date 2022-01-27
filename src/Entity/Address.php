3e5bb6238a71f783319f68793edad80c4a721574
Gaayaomar <o.gaaya1109@myu.universitecentrale.tn>
2022-01-27 22:44:10 +0100

    final commit

---
+++ src/Entity/Address.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AddressRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=AddressRepository::class)
+ */
+class Address
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=User1::class, inversedBy="addresses")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $user;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUser(): ?User1
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User1 $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+}
